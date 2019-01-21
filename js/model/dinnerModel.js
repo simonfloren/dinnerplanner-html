4df9893c0a9bb141f22608588f41443e2623988d
Albin Winkelmann <albin-w@hotmail.com>
2019-01-21 12:36:19 +0100

    First part of the lab done

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -3,50 +3,264 @@ var DinnerModel = function() {
  
 	//TODO Lab 1 implement the data structure that will hold number of guest
 	// and selected dishes for the dinner menu
-
-
+	var numberOfGuests = 0;
+	var menu = [{
+		'id':1,
+		'name':'French toast',
+		'type':'starter',
+		'image':'toast.jpg',
+		'description':"In a large mixing bowl, beat the eggs. Add the milk, brown sugar and nutmeg; stir well to combine. Soak bread slices in the egg mixture until saturated. Heat a lightly oiled griddle or frying pan over medium high heat. Brown slices on both sides, sprinkle with cinnamon and serve hot.",
+		'ingredients':[{ 
+			'name':'eggs',
+			'quantity':0.5,
+			'unit':'',
+			'price':10
+			},{
+			'name':'milk',
+			'quantity':30,
+			'unit':'ml',
+			'price':6
+			},{
+			'name':'brown sugar',
+			'quantity':7,
+			'unit':'g',
+			'price':1
+			},{
+			'name':'ground nutmeg',
+			'quantity':0.5,
+			'unit':'g',
+			'price':12
+			},{
+			'name':'white bread',
+			'quantity':2,
+			'unit':'slices',
+			'price':2
+			}]
+		},{
+		'id':2,
+		'name':'Sourdough Starter',
+		'type':'starter',
+		'image':'sourdough.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'active dry yeast',
+			'quantity':0.5,
+			'unit':'g',
+			'price':4
+			},{
+			'name':'warm water',
+			'quantity':30,
+			'unit':'ml',
+			'price':0
+			},{
+			'name':'all-purpose flour',
+			'quantity':15,
+			'unit':'g',
+			'price':2
+			}]
+		},{
+		'id':3,
+		'name':'Baked Brie with Peaches',
+		'type':'starter',
+		'image':'bakedbrie.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'round Brie cheese',
+			'quantity':10,
+			'unit':'g',
+			'price':8
+			},{
+			'name':'raspberry preserves',
+			'quantity':15,
+			'unit':'g',
+			'price':10
+			},{
+			'name':'peaches',
+			'quantity':1,
+			'unit':'',
+			'price':4
+			}]
+		},{
+		'id':100,
+		'name':'Meat balls',
+		'type':'main dish',
+		'image':'meatballs.jpg',
+		'description':"Preheat an oven to 400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes.",
+		'ingredients':[{ 
+			'name':'extra lean ground beef',
+			'quantity':115,
+			'unit':'g',
+			'price':20
+			},{
+			'name':'sea salt',
+			'quantity':0.7,
+			'unit':'g',
+			'price':3
+			},{
+			'name':'small onion, diced',
+			'quantity':0.25,
+			'unit':'',
+			'price':2
+			},{
+			'name':'garlic salt',
+			'quantity':0.7,
+			'unit':'g',
+			'price':2
+			},{
+			'name':'Italian seasoning',
+			'quantity':0.6,
+			'unit':'g',
+			'price':3
+			},{
+			'name':'dried oregano',
+			'quantity':0.3,
+			'unit':'g',
+			'price':3
+			},{
+			'name':'crushed red pepper flakes',
+			'quantity':0.6,
+			'unit':'g',
+			'price':3
+			},{
+			'name':'Worcestershire sauce',
+			'quantity':6,
+			'unit':'ml',
+			'price':7
+			},{
+			'name':'milk',
+			'quantity':20,
+			'unit':'ml',
+			'price':4
+			},{
+			'name':'grated Parmesan cheese',
+			'quantity':5,
+			'unit':'g',
+			'price':8
+			},{
+			'name':'seasoned bread crumbs',
+			'quantity':15,
+			'unit':'g',
+			'price':4
+			}]
+		},{
+		'id':101,
+		'name':'MD 2',
+		'type':'main dish',
+		'image':'bakedbrie.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'ingredient 1',
+			'quantity':1,
+			'unit':'pieces',
+			'price':8
+			},{
+			'name':'ingredient 2',
+			'quantity':15,
+			'unit':'g',
+			'price':7
+			},{
+			'name':'ingredient 3',
+			'quantity':10,
+			'unit':'ml',
+			'price':4
+			}]
+		}];
+	var totalPrice = 0;
+	
 	this.setNumberOfGuests = function(num) {
-		//TODO Lab 1
+		// By assumption num will always be 1 or -1
+		numberOfGuests += num;
 	}
 	
 	this.getNumberOfGuests = function() {
 		//TODO Lab 1
+		return numberOfGuests;
 	}
 
-	//Returns the dish that is on the menu for selected type 
+	/** BAD WRITING: Returns the dish that is on the menu for selected type
+	 *  Returns the dish from the dinner menu that meets the `selected type`
+	*/
 	this.getSelectedDish = function(type) {
-		//TODO Lab 1
+		for (var i = 0; i < menu.length; i++) {
+			if (menu[i].type == type) {
+				return menu[i];
+			}
+		}
 	}
 
-	//Returns all the dishes on the menu.
+	/** Returns all the dishes on the menu. */
 	this.getFullMenu = function() {
 		//TODO Lab 1
+		return menu;
 	}
 
-	//Returns all ingredients for all the dishes on the menu.
+	/** Returns all ingredients for all the dishes on the menu. */
 	this.getAllIngredients = function() {
-		//TODO Lab 1
+		var ingredients = [];
+		menu.forEach(value => {
+			ingredients.push(...value.ingredients);
+		});
+		return ingredients;
 	}
 
-	//Returns the total price of the menu (all the ingredients multiplied by number of guests).
+	/** Forces update of total menu price */
+	this.updateMenuPrice = function() {
+		totalPrice = 0;
+		this.getAllIngredients().forEach(ingredient => {
+			totalPrice += ingredient.price;
+		});
+	}
+
+	/** Returns the total price of the menu (all the ingredients multiplied by number of guests). */
 	this.getTotalMenuPrice = function() {
 		//TODO Lab 1
+		return totalPrice;
 	}
 
-	//Adds the passed dish to the menu. If the dish of that type already exists on the menu
-	//it is removed from the menu and the new one added.
-	this.addDishToMenu = function(id) {
-		//TODO Lab 1 
+ 	/** Takes a dish and return the total price based on the ingredients */
+	this.getDishPrice = function(dish) {
+		const ingredients = dish.ingredients;
+		var price = 0;
+		ingredients.forEach(currentIngredient => {
+			price += currentIngredient.price;
+		});
+		return price;
 	}
 
-	//Removes dish from menu
+	/** function that returns a dish of specific ID */
+	this.getDish = function (id) {
+		for(key in dishes){
+			if(dishes[key].id == id) {
+				return dishes[key];
+			}
+		}
+	}
+
+	/** Removes dish from menu */ 
 	this.removeDishFromMenu = function(id) {
-		//TODO Lab 1
+		menu = menu.filter(function(currentDish){
+			return currentDish.id != id;
+		});
 	}
 
-	//function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
-	//you can use the filter argument to filter out the dish by name or ingredient (use for search)
-	//if you don't pass any filter all the dishes will be returned
+	/** Adds the passed dish to the menu. If the dish of that type already exists on the menu
+	 * it is removed from the menu and the new one added. */
+	this.addDishToMenu = function(id) {
+		var newDish = this.getDish(id);
+		var prevDish = this.getSelectedDish(newDish.type);		
+		if (typeof prevDish !== 'undefined') {
+			this.removeDishFromMenu(prevDish.id);
+			totalPrice -= this.getDishPrice(prevDish);
+		}
+		menu.push(newDish);
+		
+		totalPrice += this.getDishPrice(newDish);
+	}	
+
+	/** function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
+	 * you can use the filter argument to filter out the dish by name or ingredient (use for search)
+	 * if you don't pass any filter all the dishes will be returned
+	 */
 	this.getAllDishes = function (type,filter) {
 	  return dishes.filter(function(dish) {
 		var found = true;
@@ -57,24 +271,14 @@ var DinnerModel = function() {
 					found = true;
 				}
 			});
-			if(dish.name.indexOf(filter) != -1)
-			{
+
+			if(dish.name.indexOf(filter) != -1) {
 				found = true;
 			}
 		}
 	  	return dish.type == type && found;
 	  });	
 	}
-
-	//function that returns a dish of specific ID
-	this.getDish = function (id) {
-	  for(key in dishes){
-			if(dishes[key].id == id) {
-				return dishes[key];
-			}
-		}
-	}
-
 
 	// the dishes variable contains an array of all the 
 	// dishes in the database. each dish has id, name, type,
