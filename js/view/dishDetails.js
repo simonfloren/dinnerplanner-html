fdf059f3d00de61cae87995642d64cedf66f0e42
Simon Tran FLorén <simon.floren@gmail.com>
2019-01-27 22:23:04 +0100

    changed structure

--- js/view/dishDetails.js
+++ js/view/dishDetails.js
@@ -1,23 +1,35 @@
 var DishDetails = function (container, model) {
-  this.image = container.find('#detail-img');
+
+    // Simple
+    this.update = function (model) {
+        guests.val(model.getNumberOfGuests());
+    }
+
+    this.removeView = function () {
+        model.removeObserver(this.update);
+    }
+
+    if (typeof container === 'undefined') {
+        console.error("Undefined container");
+        return;
+    }
+    console.info("[dishDetails] Initializing..");
+
+    this.image = container.find('#detail-img');
+    image.src=""; // Send in data here, method to do so not implemented yet
 
     var name = container.find('#dishName');
     name.html(); // Send in data here, method to do so not implemented yet
 
     var description = container.find('#description');
-    description.html();
-
-  var ingredients = container.find('#ingredient-table');
-
-    var price = container.find('#dish-price');
+    description.html(); // Send in data here, method to do so not implemented yet
 
     var guests = container.find('#numberOfGuests');
     guests.val(model.getNumberOfGuests());
 
-  // Simple
-  this.update = function (model) {
-    guests.val(model.getNumberOfGuests());
-  }
+    var ingredients = container.find('#ingredient-table');
 
-  model.addObserver(this.update);
+    var price = container.find('#dish-price');
+
+    model.addObserver(this.update);
 };
