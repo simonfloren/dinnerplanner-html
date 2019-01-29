01c87217c92591067eefd158be783597dee59070
Simon Tran FLorén <simon.floren@gmail.com>
2019-01-29 22:25:04 +0100

    removed some jquery

--- js/view/dishSearchView.js
+++ js/view/dishSearchView.js
@@ -20,9 +20,9 @@ class DishSearchView {
     this.model = model;
     this.container = container;
 
-    let keyWordsAttribute = container.find('#keyWords');
-    let selectBox = container.find('#dishTypeSelect');
-    let dishContainer = container.find('#dishSearchBody');
+    let keyWordsAttribute = container.querySelector('#keyWords');
+    let selectBox = container.querySelector('#dishTypeSelect');
+    let dishContainer = container.querySelector('#dishSearchBody');
 
     //console.log("Dish container", dishContainer);
   }
