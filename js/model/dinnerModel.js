61f448e12ac59776821ae2de763d93c7f64a1420
Albin Winkelmann <albin-w@hotmail.com>
2019-02-06 20:01:34 +0100

    Minor changes

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -11,9 +11,6 @@ let menu = [];
 // Base api url
 const baseURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com";
 
-// API Key
-const API_KEY = "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767";
-
 class DinnerModel {
 	constructor() {
 		this.dishes; // declade dishes value
